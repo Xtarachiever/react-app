f0c9bc6daee41e80b93d3e39be1f16ab2b8f428a
Adeyemi Esther <63353240+Xtarachiever@users.noreply.github.com>
2020-09-07 02:46:12 +0100

    Updates

---
+++ precache-manifest.7ed67c33a4248bd8022744742e90e326.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c1d891683ef71d54e95283df957211",
+    "url": "/Deployment/index.html"
+  },
+  {
+    "revision": "af51c574fe35210dc44f",
+    "url": "/Deployment/static/css/2.e50548bd.chunk.css"
+  },
+  {
+    "revision": "cbad90f02e6a37c0ee58",
+    "url": "/Deployment/static/css/main.d6bcb662.chunk.css"
+  },
+  {
+    "revision": "af51c574fe35210dc44f",
+    "url": "/Deployment/static/js/2.a30e128b.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Deployment/static/js/2.a30e128b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbad90f02e6a37c0ee58",
+    "url": "/Deployment/static/js/main.99d712ff.chunk.js"
+  },
+  {
+    "revision": "0166d4c40b1ef39fe4e9",
+    "url": "/Deployment/static/js/runtime-main.dec2d299.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Deployment/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Deployment/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Deployment/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Deployment/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Deployment/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
